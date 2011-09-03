c727a6be607b63b930c37aae0115f7b4c987d5f8
U-stefan-laptop\stefan <stefan@stefan-laptop.(none)>
2011-09-03 17:14:29 -0400

    Incremented version to 0.3.0

--- CompassScssIntegrationGrailsPlugin.groovy
+++ CompassScssIntegrationGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class CompassScssIntegrationGrailsPlugin {
-    def version = "0.2.8"
+    def version = "0.3.0"
     def grailsVersion = "1.3.7 > *"
     def dependsOn = [:]
     def pluginExcludes = [
