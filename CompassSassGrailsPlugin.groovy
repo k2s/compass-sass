710a43581718003061068c95a66c3e50434ea8cd
Stefan Kendall <stefankendall@gmail.com>
2011-10-25 01:46:56 -0400

    Setting version to 0.5.2 and republishing.

--- CompassSassGrailsPlugin.groovy
+++ CompassSassGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class CompassSassGrailsPlugin {
-    def version = "0.5.1"
+    def version = "0.5.2"
     def grailsVersion = "1.3.7 > *"
     def dependsOn = [resources: '1.0.2']
     def pluginExcludes = [
