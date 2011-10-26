5532451c50d3fde6cf32e9dd7761f6db33ec32e9
Stefan Kendall <stefankendall@gmail.com>
2011-10-25 21:09:26 -0400

    Republishing as 0.5.4

--- CompassSassGrailsPlugin.groovy
+++ CompassSassGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class CompassSassGrailsPlugin {
-    def version = "0.5.3"
+    def version = "0.5.4"
     def grailsVersion = "1.3.7 > *"
     def dependsOn = [resources: '1.0.2 > *']
     def pluginExcludes = [
